67eba1e3847caacdbe4c101065dc3b2ef479dfb7
Brian Link <cpsubrian@gmail.com>
2011-09-23 11:05:19 -0700

    Moves scripts from conf.js to helpers.js.

--- helpers.js
+++ helpers.js
@@ -2,23 +2,43 @@
  * Application helpers.  These are available in all rendered template.
  */
 module.exports = function(app, conf) {
+  var scripts = {
+    'head': [
+      'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js',
+      //'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js',
+      'http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.1.7/underscore-min.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.5.1/backbone-min.js',
+      //'https://github.com/documentcloud/backbone/raw/master/backbone.js',
+      //'http://cdnjs.cloudflare.com/ajax/libs/socket.io/0.7.0/socket.io.min.js',
+      //'http://html5shim.googlecode.com/svn/trunk/html5.js',
+    ],
+    'footer': [
+      '/js/script.js'       
+    ]
+  };
+  
+  /* Static helpers. */
   app.helpers({
     // Application title.
     title: conf.title,  
     
     // Return scripts from settings.js
     headScripts: function() {
-      return conf.scripts['head'].map(function(src) {
+      return scripts['head'].map(function(src) {
         return '<script type="text/javascript" src="' + src + '"></script>'
       }).join("\n");
     },
     footerScripts: function(req, res) {
-      return conf.scripts['footer'].map(function(src) {
+      return scripts['footer'].map(function(src) {
         return '<script type="text/javascript" src="' + src + '"></script>'
       }).join("\n");
     }
   });
+  
+  /* Dynamic helpers. */
   app.dynamicHelpers({
+    // Output whether the current user is authenticated or not in a script tag.
     authGlobal: function(req, res) {
       return '<script type="text/javascript">var authenticated = ' + (req.user ? 'true' : 'false') + '</script>'; 
     }
